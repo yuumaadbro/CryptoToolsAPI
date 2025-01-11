e3cbaf0427f2b571e22563baf17c67de1c8e316e
Jesús Blanco Astilleros <jblanco@fpmaragall.org>
2025-01-11 02:46:57 +0100

    Forgot Status 403

---
+++ CryptoToolsAPI/DTOs/HttpResponses/Status403DTO.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptoToolsAPI.DTOs.HttpResponses
+{
+    public class Status403DTO
+    {
+        public int StatusCode { get; set; } = 403;
+        public string StatusMessage { get; set; } = "Forbidden, your user does not have acces to this resource.";
+    }
+}
